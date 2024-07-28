222cb48fc32743efaaae1a613dfeffb3ee1ae4e7
Dibakar <dibakarbera01@gmail.com>
2024-07-28 18:05:30 +0530

    about new design and previous code commented

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,7 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import './About.css'
-import theme_pattern from '../../assets/theme_pattern.svg'
+// import theme_pattern from '../../assets/theme_pattern.svg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
 
 import about_pic from "../../assets/about1.png"
 
@@ -10,7 +12,7 @@ import about_pic from "../../assets/about1.png"
 const About = () => {
   return (
     <>
-      <div id='about' className='about'>
+      {/* <div id='about' className='about'>
         <div className="title-box">
           <h1>About me</h1>
           <img src={theme_pattern} alt="" />
@@ -89,6 +91,104 @@ const About = () => {
           </div>
 
         </div>
+
+      </div> */}
+
+      <div className="bg-neutral-900">
+        <div className="max-w-5xl px-4 xl:px-0 py-10 lg:pt-20 lg:pb-20 mx-auto">
+          <div className="max-w-3xl mb-10 lg:mb-14">
+            <h2 className="text-white font-semibold text-2xl md:text-4xl md:leading-tight">About Us</h2>
+            <p className="mt-1 text-neutral-400">Welcome to our online e-commerce store! We are thrilled to have you here and offer you a wide range of high-quality products at competitive prices. Our goal is to provide you with an easy and enjoyable shopping experience from the comfort of your own home.</p>
+          </div>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-16 lg:items-center">
+            <div className="aspect-w-16 aspect-h-9 lg:aspect-none">
+              <img className="w-full object-cover rounded-xl" src={about_pic} alt="Image Description" />
+            </div>
+            <div>
+              <div className="mb-4">
+                <h2 className="text-2xl font-medium  text-[#ff0]">
+                Why Buy From Us?
+                </h2>
+              </div>
+              <div className="flex gap-x-5 ms-1">
+
+                <div className="relative last:after:hidden after:absolute after:top-8 after:bottom-0 after:start-4 after:w-px after:-translate-x-[0.5px] after:bg-neutral-800">
+                  <div className="relative z-10 size-8 flex justify-center items-center">
+                    <span className="flex flex-shrink-0 justify-center items-center size-8 border border-neutral-800 text-[#ff0] font-semibold text-xs uppercase rounded-full">
+                      1
+                    </span>
+                  </div>
+                </div>
+                <div className="grow pt-0.5 pb-8 sm:pb-12">
+                  <p className="text-sm lg:text-base text-neutral-400">
+                    <span className="text-white">Instant Inspiration: </span>
+                    Download project files instantly and jumpstart your music production with fresh ideas.
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-x-5 ms-1">
+
+                <div className="relative last:after:hidden after:absolute after:top-8 after:bottom-0 after:start-4 after:w-px after:-translate-x-[0.5px] after:bg-neutral-800">
+                  <div className="relative z-10 size-8 flex justify-center items-center">
+                    <span className="flex flex-shrink-0 justify-center items-center size-8 border border-neutral-800 text-[#ff0] font-semibold text-xs uppercase rounded-full">
+                      2
+                    </span>
+                  </div>
+                </div>
+                <div className="grow pt-0.5 pb-8 sm:pb-12">
+                  <p className="text-sm lg:text-base text-neutral-400">
+                    <span className="text-white">Secure Checkout: </span>
+                    Shop with confidence through our encrypted payment gateway.
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-x-5 ms-1">
+                <div className="relative last:after:hidden after:absolute after:top-8 after:bottom-0 after:start-4 after:w-px after:-translate-x-[0.5px] after:bg-neutral-800">
+                  <div className="relative z-10 size-8 flex justify-center items-center">
+                    <span className="flex flex-shrink-0 justify-center items-center size-8 border border-neutral-800 text-[#ff0] font-semibold text-xs uppercase rounded-full">
+                      3
+                    </span>
+                  </div>
+                </div>
+                <div className="grow pt-0.5 pb-8 sm:pb-12">
+                  <p className="text-sm md:text-base text-neutral-400">
+                    <span className="text-white">Global Access: </span>
+                    If you&apos;re not happy with your purchase, we&apos;ll make it right.
+
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-x-5 ms-1">
+
+                <div className="relative last:after:hidden after:absolute after:top-8 after:bottom-0 after:start-4 after:w-px after:-translate-x-[0.5px] after:bg-neutral-800">
+                  <div className="relative z-10 size-8 flex justify-center items-center">
+                    <span className="flex flex-shrink-0 justify-center items-center size-8 border border-neutral-800 text-[#ff0] font-semibold text-xs uppercase rounded-full">
+                      4
+                    </span>
+                  </div>
+                </div>
+                <div className="grow pt-0.5 pb-8 sm:pb-12">
+                  <p className="text-sm md:text-base text-neutral-400">
+                    <span className="text-white">Satisfaction Guarantee: </span>
+                    Wherever you are, our digital store is just a click away.
+                  </p>
+                </div>
+              </div>
+              <AnchorLink  offset={50} href='#contact'>
+              <div className="group inline-flex items-center gap-x-2 py-2 px-3 bg-[#ff0] font-medium text-sm text-neutral-800 rounded-full focus:outline-none" href="/contact">
+             
+                <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path><path className="opacity-0 group-hover:opacity-100 group-focus:opacity-100 group-hover:delay-100 transition" d="M14.05 2a9 9 0 0 1 8 7.94"></path><path className="opacity-0 group-hover:opacity-100 group-focus:opacity-100 transition" d="M14.05 6A5 5 0 0 1 18 10"></path></svg>
+                Schedule a call
+                
+              </div>
+              </AnchorLink>
+            </div>
+
+          </div>
+        </div>
+
+
+
 
       </div>
 
